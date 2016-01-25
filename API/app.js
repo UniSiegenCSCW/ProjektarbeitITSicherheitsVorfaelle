bf3763bce5d467329ba4d94371c422b93f475280
hoeseb <Basti@Bastis-MacBook-Pro.local>
2016-01-25 15:16:01 +0100

    first API commit

--- API/app.js
+++ API/app.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var request = require('request');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/autocomplete', function(req, res){
-	console.log(req.body);
-	res.send('klappt!');
+app.post('/autocompleteVendor', function(req, res){
+	var vendor = req.body.vendor;
+	var productArr;
+	var productArrReplaced = [];
+	
+	var options = {
+	  uri: 'http://cve.circl.lu/api/browse/' + vendor,
+	  method: 'GET'
+	};
+
+	request(options, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+		  var jsonBody = JSON.parse(body);
+		productArr = jsonBody.product;		
+		for (var index in productArr) {
+			var repl = productArr[index].replace("_", " ");
+			productArrReplaced.push(repl);
+			//console.log(repl);
+		}
+		console.log(productArrReplaced);
+		res.send(productArrReplaced);
+	  }
+	});
 })
 
 
