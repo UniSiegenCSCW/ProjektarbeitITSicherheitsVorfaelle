cea0349a8b1a33e3b894e2e1c1c68daf3d53369e
hoeseb <Basti@Bastis-MacBook-Pro.local>
2016-02-05 15:14:06 +0100

    cve search

--- API/app.js
+++ API/app.js
@@ -12,6 +12,96 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.post('/cveSearch', function(req, res){
+	var cveArr;
+	var vendor = req.body.vendor;
+	var product = req.body.product;
+	var limit = parseInt(req.body.limit);
+	var since = parseInt(req.body.since);
+	var cwe = String(req.body.cwe);
+	var cvss = parseInt(req.body.cvss);
+
+	console.log(vendor + product);
+
+	var options = {
+		uri: 'http://cve.circl.lu/api/search/' + vendor + '/' + product,
+		method: 'GET'
+	};
+
+	var listLimit = 5;
+	if (Number.isInteger(limit))
+	{
+		listLimit = limit;
+	}
+
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode == 200){
+			var jsonBody = JSON.parse(body);
+			var resultList = [];
+			var count = 0;
+
+			var isSince = Number.isInteger(since);
+			var isLimit = Number.isInteger(limit);
+			var isCwe = (cwe.indexOf('CWE') != -1);
+			var isCvss = Number.isInteger(cvss);
+			var isFiltered = isSince || isCwe || isCvss;
+			
+			for(var x in jsonBody){
+				var element = jsonBody[x];
+				
+				if(isFiltered)
+				{
+					if(isSince)
+					{
+						var modified = new Date(element["Modified"]);
+						var now = Date.now();
+						var week = since * 24 * 60 * 60 * 1000;
+						
+						if ((now - modified) < week){
+							resultList.push(element);
+						}
+					}
+					if(isCwe)
+					{
+						var elementCwe = String(element["cwe"]);
+						if(elementCwe.indexOf(cwe) != -1 || elementCwe.indexOf('Unknown') != -1)
+						{
+							resultList.push(element);
+						}
+					}
+
+					if(isCvss)
+					{
+						if(parseInt(element["cvss"]) > cvss)
+						{
+							resultList.push(element);
+						}
+					}
+
+					if(isLimit)
+					{	
+						if(resultList.length >= listLimit)
+						{
+							break;
+						}
+					}
+				}
+				else
+				{
+					resultList.push(element);
+
+					if(resultList.length >= listLimit)
+					{
+						break;
+					}
+				}
+			}
+
+			res.send(resultList);
+		}
+	});
+})
+
 app.post('/autocompleteVendor', function(req, res){
 	var vendorArr;
 	var vendorArrReplaced = [];
@@ -27,15 +117,13 @@ app.post('/autocompleteVendor', function(req, res){
 	  	if (!error && response.statusCode == 200) {
 		  	var jsonBody = JSON.parse(body);
 			vendorArr = jsonBody.vendor;	
-			//console.log(vendorArr);	
 			for (var index in vendorArr) {
 				var repl = vendorArr[index].replace(/_/g, " ");
 				if (repl.indexOf(filter) != -1) {
 					vendorArrReplaced.push(repl);
-					//console.log(repl);
 				}
 			}
-			//console.log(productArrReplaced);
+
 			res.send(vendorArrReplaced);
 	  		}
 		});
@@ -62,7 +150,6 @@ app.post('/autocompleteProduct', function(req, res){
 		for (var index in productArr) {
 			var repl = productArr[index].replace(/_/g, " ");
 			productArrReplaced.push(repl);
-			//console.log(repl);
 		}
 		console.log(productArrReplaced);
 		res.send(productArrReplaced);
